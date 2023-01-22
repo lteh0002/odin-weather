9debfbdb6ac866277ba46b16add90d8effd3ef3c
lteh0002 <tehlinhui@gmail.com>
2023-01-22 15:44:27 +0800

    Print current weather for a certain city in frontend

---
+++ api.js
@@ -0,0 +1,3 @@
+const API_KEY = '2637a4df1d01664d70473ea8500bc53f'
+
+export default API_KEY
--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-import * as env from 'dotenv'
-env.config()
+// import env from '/dotenv'
+// // const env = require('dotenv')
+// env.config()
+
+import API_KEY from "./api.js"
+
+const printWeather = document.querySelector('.weather')
+const printTemp = document.querySelector('.temperature')
 
 async function getLocation() {
-    const data = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Kuala+Lumpur&limit=5&appid=${process.env.API_KEY}`)
+    const data = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Paris&limit=5&appid=${API_KEY}`)
     const result = await data.json()
     return [result[0].lat, result[0].lon]
 }
 
-
 async function fetchWeather() {
     const [lat, lon] = await getLocation()
-    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${process.env.API_KEY}`)
+    const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=2637a4df1d01664d70473ea8500bc53f`)
     const result = await data.json()
-    console.log(result.main.temp)
-}
+    console.log(result)
+    // console.log(result.weather[0].main)
+    // console.log(result.main.temp)
+    printWeather.textContent = result.weather[0].main 
+    printTemp.textContent = Math.floor(result.main.temp)
 
+}
 
 fetchWeather()
