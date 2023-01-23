93d86334701fdffbebbdd2ff8f20afa75f65313a
lteh0002 <tehlinhui@gmail.com>
2023-01-23 11:33:03 +0800

    Show current weather and temperature

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 // // const env = require('dotenv')
 // env.config()
 
+// import { format, compareAsc } from 'date-fns'
 import API_KEY from "./hide/api.js"
 
-const printWeather = document.querySelector('.weather')
-const printTemp = document.querySelector('.temperature')
-const searchBar = document.querySelector('#searchbar')
+const printWeather = document.querySelector('.today-weather')
+const printTemp = document.querySelector('.today-temperature')
+const todayicon = document.querySelector('.todayicon')
+const todayHumid = document.querySelector('.humidity')
+const location = document.querySelector('.location')
+
+// const todayDate = format(new Date(), 'dd/MMM/yyyy')
 
 async function getLocation() {
     const data = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=Sungai+Siput&limit=5&appid=${API_KEY}`)
@@ -21,9 +26,17 @@ async function fetchWeather() {
     console.log(result)
     // console.log(result.weather[0].main)
     // console.log(result.main.temp)
+    location.textContent = `${result.name}` + `, ` + `${result.sys.country}`
     printWeather.textContent = result.weather[0].main 
-    printTemp.textContent = Math.floor(result.main.temp)
+    printTemp.textContent = `${Math.floor(result.main.temp)}°`
+    todayHumid.textContent = `${result.main.humidity} %`
+    iconDisplay(result.weather[0].main)
+}
+
+function iconDisplay(weather) {
+    if (weather === 'Clouds') {
+        todayicon.textContent = 'partly_cloudy_night'
+    }
 }
 
 fetchWeather()
-searchCity()
